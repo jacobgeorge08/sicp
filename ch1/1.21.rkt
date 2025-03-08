b85214271768fe7137371e1ed5be181fc6075614
T460 <jacobgeorge08@gmail.com>
2025-03-08 18:04:07 +0530

    finished 1.21

---
+++ ch1/1.21.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+; Exercise 1.21: Use the smallest-divisor procedure to ﬁnd the smallest divisor of each of the following numbers: 199, 1999, 19999.
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square x)
+  ( * x x))
+
+(smallest-divisor 199)
+;199
+(smallest-divisor 1999)
+;1999
+(smallest-divisor 19999)
+;7
+
+; 199 and 1999 being prime was not on my bingo cards
+;
