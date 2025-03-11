f7cfc38e0d2863eed069ef61f523c749217b3544
T460 <jacobgeorge08@gmail.com>
2025-03-11 08:07:52 +0530

    finished 1.25

---
+++ ch1/1.25.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+
+(define (expmod-Alyssa base exp m)
+  (remainder (fast-expt base exp) m))
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square x)
+  (* x x))
+
+; Alyssa works but its slow af compared to the regular expmod from our boy Fermat
+
