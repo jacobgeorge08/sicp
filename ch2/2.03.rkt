24a0e41ab79448f1859f9b191da5d099ec03c873
T460 <jacobgeorge08@gmail.com>
2025-03-16 19:12:59 +0530

    solved 2.03
    My two variations of rectangles are quite simple. Also I should've
    probably used (abs) while calcuating the differences between points

---
+++ ch2/2.03.rkt
@@ -0,0 +1,41 @@
+#lang sicp
+
+(define (make-point x y)
+  (cons x y))
+(define (x-point p)
+  (car p))
+(define (y-point p)
+  (cdr p))
+
+(define (make-segment start end)
+  (cons start end))
+(define (start-segment x)
+  (car x))
+(define (end-segment x)
+  (cdr x))
+
+(define x (make-point 2 3))
+(define y (make-point 12 13))
+
+(define (perimeter rect)
+  (+ (* 2 (car rect)) (* 2 (cdr rect))))
+(define (area rect)
+  (* (car rect) (cdr rect)))
+
+(define (segment-rectangle l-seg b-seg)
+  (let ((l (- (x-point(end-segment l-seg)) (x-point(start-segment l-seg))))
+        (b (- (y-point(end-segment b-seg)) (y-point(start-segment b-seg)))))
+    (cons l b)))
+
+(define seg1 (make-segment (make-point 1 2) (make-point 6 2)))
+(define seg2 (make-segment (make-point 6 2) (make-point 6 5)))
+
+(define seg-rect (segment-rectangle seg1 seg2))
+
+(define (point-rectange l-start-p l-end-p b-start-p b-end-p)
+  (let ((l (- (x-point l-end-p) (x-point l-start-p)))
+        (b (- (y-point b-end-p) (y-point b-start-p))))
+    (cons l b)))
+
+(define point-rect (point-rectange (make-point 1 2)(make-point 6 2)(make-point 6 2)(make-point 6 5)))
+
