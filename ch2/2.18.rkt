7dc642ea8c034433ad96a3d1ff8f4c6435da2f7e
T460 <jacobgeorge08@gmail.com>
2025-03-22 12:15:32 +0530

    solved 2.18

---
+++ ch2/2.18.rkt
@@ -0,0 +1,16 @@
+#lang sicp
+
+(define (reverse-recur items)
+  (if (null? items)
+    nil
+    (append (reverse-recur (cdr items)) (list (car items) ))))
+
+(define (reverse items)
+  (define (reverse-iter remaining result)
+    (if (null? remaining)
+      result
+      (reverse-iter (cdr remaining) (cons (car remaining) result))))
+    (reverse-iter items nil))
+
+(reverse (list 1 4 9 16 25))
+(reverse-recur (list 1 4 9 16 25))
