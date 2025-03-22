ee8776bcb6e1a54154ef7c50f7d385768811d70a
T460 <jacobgeorge08@gmail.com>
2025-03-22 12:15:43 +0530

    solved 2.21

---
+++ ch2/2.21.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+
+(define (square x)
+  (* x x))
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (square (car items)) (square-list_ (cdr items)))))
+
+(define (square-list items)
+  (map square items))
+
+(square-list (list 1 2 3 4))
+
