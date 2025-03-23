1847ae695d09d1542c8d9b09eea4f73a0ad489b9
T460 <jacobgeorge08@gmail.com>
2025-03-24 04:08:17 +0530

    added iterative solution

--- ch2/2.27.rkt
+++ ch2/2.27.rkt
@@ -1,5 +1,4 @@
 #lang sicp
-;; Exercise 2.27: Modify your reverse procedure of Exercise 2.18 to produce a deep-reverse procedure that takes a list as argument and returns as its value the list with its elements reversed and with all sublists deep-reversed as well. For example,
 
 (define x (list (list 1 2) (list 3 4)))
 (define y (list (list 1 2) (list (list 3 4) (list 5 6 7))))
@@ -8,10 +7,18 @@
   (cond ((null? items) nil)
         ((not (pair? items)) items)
         (else (append (deep-reverse (cdr items)) (list (deep-reverse (car items)))))))
+
+(define (dreverse items)
+  (define (dreverse-iter remaining result)
+    (cond ((null? remaining) result)
+          ((not (pair? remaining)) remaining)
+          (else (dreverse-iter (cdr remaining)
+                               (cons (dreverse (car remaining)) result)))))
+  (dreverse-iter items nil))
+
 x
 (deep-reverse x)
+(dreverse x)
 y
 (deep-reverse y)
-
-
-
+(dreverse y)
