572021264fe459f91b2a8464342523142a7bad8e
T460 <jacobgeorge08@gmail.com>
2025-03-24 03:59:22 +0530

    formatted parens

--- ch2/2.18.rkt
+++ ch2/2.18.rkt
@@ -2,15 +2,15 @@
 
 (define (reverse-recur items)
   (if (null? items)
-    nil
-    (append (reverse-recur (cdr items)) (list (car items) ))))
+      nil
+      (append (reverse-recur (cdr items)) (list (car items)))))
 
 (define (reverse items)
   (define (reverse-iter remaining result)
     (if (null? remaining)
-      result
-      (reverse-iter (cdr remaining) (cons (car remaining) result))))
-    (reverse-iter items nil))
+        result
+        (reverse-iter (cdr remaining) (cons (car remaining) result))))
+  (reverse-iter items nil))
 
 (reverse (list 1 4 9 16 25))
 (reverse-recur (list 1 4 9 16 25))
