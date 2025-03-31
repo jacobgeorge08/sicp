6d4c2daf49d2c7d8bfce11567e9b59d9a867fb06
T460 <jacobgeorge08@gmail.com>
2025-03-31 07:29:16 +0530

    needed help from Bill The Lizard

---
+++ ch2/2.37.rkt
@@ -0,0 +1,41 @@
+#lang sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (row) (dot-product row v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row) (matrix-*-vector cols row)) m)))
+
+(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+(transpose s)
+
+(define v (list 1 3 -5))
+(define w (list 4 -2 -1))
+(dot-product v w)
+
+(define m (list (list 1 2 3) (list 4 5 6)))
+(define n (list 1 2 3))
+(matrix-*-vector m n)
+
+(define a (list (list 14 9 3) (list 2 11 15) (list 0 12 17) (list 5 2 3)))
+(define b (list (list 12 25) (list 9 10) (list 8 5)))
+(matrix-*-matrix a b)
