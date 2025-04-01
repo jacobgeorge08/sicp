72cdebba8b47bc245192cbcb587a060337cf3844
T460 <jacobgeorge08@gmail.com>
2025-04-01 05:58:06 +0530

    solved 2.39

---
+++ ch2/2.39.rkt
@@ -0,0 +1,26 @@
+#lang sicp
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (reverseR sequence)
+  (fold-right (lambda (x y) (append y (list x)))
+              nil sequence))
+(reverseR (list 1 4 9 16 25))
+
+(define (reverseL sequence)
+  (fold-left (lambda (x y)
+               (cons y x))
+             nil sequence))
+(reverseL (list 1 4 9 16 25))
