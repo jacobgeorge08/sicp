bf9860da3300cc8004aafc3ac34f5930ce57c89d
T460 <jacobgeorge08@gmail.com>
2025-07-05 03:59:18 +0530

    formatted solution and changed '() to nil

--- ch2/2.35.rkt
+++ ch2/2.35.rkt
@@ -7,7 +7,7 @@
           (accumulate op initial (cdr sequence)))))
 
 (define (enumerate-tree tree)
-  (cond ((null? tree) '())
+  (cond ((null? tree) nil)
         ((not (pair? tree)) (list tree))
         (else (append (enumerate-tree (car tree))
                       (enumerate-tree (cdr tree))))))
@@ -16,8 +16,9 @@
   (accumulate + 0 (map (lambda (x) 1) (enumerate-tree t))))
 
 (define tree (list 1 2 (list 3 4 (list 5 6)) 7))
-(enumerate-tree (list (list 1 2) (list 1 2 3) 1))
-(count-leaves (list (list 1 2) (list 1 2 3) 1))
 (enumerate-tree tree)
 (count-leaves tree)
+
+(enumerate-tree (list (list 1 2) (list 1 2 3) 1))
+(count-leaves (list (list 1 2) (list 1 2 3) 1))
 
