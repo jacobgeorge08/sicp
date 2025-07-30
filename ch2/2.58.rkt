625a7f4ff6a1dc689710e471f22839cd90426cc1
T460 <jacobgeorge08@gmail.com>
2025-07-30 17:15:10 +0530

    Grokked part 2
    Key thing to figure out was how algebraic precedence works
    - if there's a + in the expression, it is a sum
    - the expression is only a product if there is a * and no + in the top
    level expression

--- ch2/2.58.rkt
+++ ch2/2.58.rkt
@@ -12,13 +12,27 @@
 (define (=number? exp y)
   (and (number? exp) (= exp y)))
 
-(define (sum? exp)
-  (and (pair? exp) (eq? (cadr exp) '+)))
-(define (addend exp)
-  (car exp))
+(define (has op expr)
+  (and (pair? expr) (memq op expr)))
 
-(define (augend exp)
-  (accumuluate make-sum 0 (cddr exp)))
+(define (unwrap expr)
+  (if (and (pair? expr) (null? (cdr expr)))
+      (car expr)
+      expr))
+
+(define (before op expr)
+  (define (iter expr)
+    (if (eq? op (car expr))
+        '()
+        (cons (car expr) (iter (cdr expr)))))
+  (unwrap (iter expr)))
+
+(define (after op expr)
+  (unwrap (cdr (memq op expr))))
+
+(define (sum? expr) (has '+ expr))
+(define (addend expr) (before '+ expr))
+(define (augend expr) (after '+ expr))
 
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
@@ -27,13 +41,9 @@
          (+ a1 a2))
         (else (list a1 '+ a2))))
 
-(define (product? exp)
-  (and (pair? exp) (eq? (cadr exp) '*)))
-(define (multiplier exp)
-  (car exp))
-
-(define (multiplicand exp)
-  (accumuluate make-product 1 (cddr exp)))
+(define (product? expr) (and (not (sum? expr)) (has '* expr)))
+(define (multiplier expr) (before '* expr))
+(define (multiplicand expr) (after '* expr))
 
 (define (make-product m1 m2)
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
@@ -41,20 +51,6 @@
         ((=number? m2 1) m1)
         ((and (number? m1) (number? m2)) (* m1 m2))
         (else (list m1 '* m2))))
-
-(define (exponentiation? exp)
-  (and (pair? exp) (eq? (cadr exp) '**)))
-(define (base exp)
-  (car exp))
-(define (exponent exp)
-  (caddr exp))
-
-(define (make-exponentiation b e)
-  (cond ((=number? e 0) 1)
-        ((=number? e 1) b)
-        ((=number? b 1) 1)
-        ((and (number? b) (number? e)) (expt b e))
-        (else (list b '** e))))
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
@@ -67,28 +63,9 @@
                         (deriv (multiplicand exp) var))
           (make-product (deriv (multiplier exp) var)
                         (multiplicand exp))))
-        ((exponentiation? exp)
-         (make-product (make-product
-                        (exponent exp)
-                        (make-exponentiation (base exp) (- (exponent exp) 1)))
-                       (deriv (base exp) var))
-         )
         (else
          (error "unknown expression type: DERIV" exp))))
 
 (deriv '(x + (3 * (x + (y + 2)))) 'x)
 (deriv '(x + 3 * (x + y + 2)) 'x)
 
-
-;; The problem becomes substantially harder if we allow standard algebraic notation, such as (x + 3 * (x + y + 2)), which drops unnecessary parentheses and assumes that multiplication is done before addition. Can you design appropriate predicates, selectors, and constructors for this notation such that our derivative program still works?
-
-;; Im thinking this is just a combination of the previous two questions
-;; The first where we remove brackets and the second where we go from
-;; prefix to infix form
-;; Going to infix form wasnt that difficult since we just change the selectors
-;;
-;; iterate through the expression and see if theres a *
-;; When I wrote an interpreter how we'd do it is,
-;; if ( , priority max
-;; if * , 2nd 
-;; if + , last
